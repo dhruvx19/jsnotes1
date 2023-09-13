f621ca355a85ef0a70a9b6c68a5991a4135e0114
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2023-09-13 13:52:12 +0000

    container

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123234
+let accountEmail = "123@gmail.com"
+let accountPassword = "123ecd"
+accountCity = "Jaipur"
+let accountState ;  //undefined
+
+
+// accountId = 4342  // not allowed
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountCity,accountEmail,accountPassword,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use script";  //  treat all JS code as newer version
+
+//alert(3+3)  //work only in browser console
+
+
+
+//null --> standalone value representation of empty value (not zero)
+//undefined --> 
