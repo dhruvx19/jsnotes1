4998b892db1e016e17227a66689154d433f59b06
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2023-12-29 17:32:25 +0530

    project

--- 07_projects/one.js
+++ 07_projects/one.js
@@ -4,4 +4,23 @@ const body = document.querySelector("body")
 
 buttons.forEach(function (button) {
     console.log(button);
-});
+    button.addEventListener('click', function(e){
+        console.log(e)
+        console.log(e.target)
+        switch (e.target.id) {
+            case 'grey':
+              body.style.backgroundColor = 'grey';
+              break;
+            case 'white':
+                body.style.backgroundColor = 'white';
+              break;
+            case 'blue':
+                body.style.backgroundColor = 'blue';
+              break;
+            case 'yellow':
+                body.style.backgroundColor = 'yellow';
+              break;
+        }
+    });
+}); 
+  
