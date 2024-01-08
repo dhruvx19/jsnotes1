b312b17da8eba5e6aa3493496469e325c4f337ad
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2024-01-08 21:14:09 +0530

    api

--- 07_projects/async/one.js
+++ 07_projects/async/one.js
@@ -12,7 +12,11 @@ const randomColor = function(){
 let intervalId;
 
 const startChangingColor = function(){
-  let intervalId =  setInterval(changeBgColor, 1000);
+   if(!intervalId){
+    intervalId =  setInterval(changeBgColor, 1000);
+
+   }
+
     function changeBgColor(){
         document.body.style.backgroundColor = randomColor();
     }
@@ -20,6 +24,7 @@ const startChangingColor = function(){
 
 const stopChangingColor = function(){
     clearInterval(intervalId);
+    intervalId = null;
 };
 
 document.querySelector('#start').addEventListener('click', startChangingColor);
